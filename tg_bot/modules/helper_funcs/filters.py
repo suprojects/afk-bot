b0949d1d641d46d2f8cbe2ec75629bc48f5b72d1
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-30 15:24:00 +0300

    Update filters.py

--- tg_bot/modules/helper_funcs/filters.py
+++ tg_bot/modules/helper_funcs/filters.py
@@ -1,16 +1,10 @@
 from telegram import Message
 from telegram.ext import BaseFilter
 
-from tg_bot import SUPPORT_USERS, SUDO_USERS
+from tg_bot import SUDO_USERS
 
 
 class CustomFilters(object):
-    class _Supporters(BaseFilter):
-        def filter(self, message: Message):
-            return bool(message.from_user and message.from_user.id in SUPPORT_USERS)
-
-    support_filter = _Supporters()
-
     class _Sudoers(BaseFilter):
         def filter(self, message: Message):
             return bool(message.from_user and message.from_user.id in SUDO_USERS)
