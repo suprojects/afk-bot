651c9b26bd38437beb1acb2153181b0b517e41ac
Roj <49933115+rojserbest@users.noreply.github.com>
2020-10-31 10:52:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,11 @@ def main():
 	
 	from afk import AFK_HANDLER, AFK_GROUP, NO_AFK_GROUP, NO_AFK_HANDLER, AFK_REPLY_HANDLER, AFK_REPLY_GROUP
 	from users import USER_HANDLER, USERS_GROUP, BROADCAST_HANDLER, CHATLIST_HANDLER
-	from start import START_HANDLER, HELP_HANDLER
+	from start import START_HANDLER, HELP_HANDLER, BTN_HANDLER
 	
 	dispatcher.add_handler(START_HANDLER)
 	dispatcher.add_handler(HELP_HANDLER)
+	dispatcher.add_handler(BTN_HANDLER)
 	dispatcher.add_handler(AFK_HANDLER, AFK_GROUP)
 	dispatcher.add_handler(NO_AFK_HANDLER, NO_AFK_GROUP)
 	dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
