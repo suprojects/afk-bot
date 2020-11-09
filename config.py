a637ae7abb5eee689810bbeebcc65846b7e1b597
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-09 14:51:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 class Config():
-    OWNER_ID = 951435494  # my telegram ID
-    OWNER_USERNAME = "su_Theta"  # my telegram username
+    SUDO_USERS = [951435494, 1178472788]
     API_KEY = "1457211149:AAHk2ZPkBP-u2UNZHoCIeHq6Hc-43KzQaeo"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass@localhost:5432/afkdb'
