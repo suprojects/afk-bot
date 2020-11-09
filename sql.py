1b20ea0a351b82a27d1be4ea9c7a98bd4ec609ca
rojserbest <rojserbest@gmail.com>
2020-11-09 11:55:53 +0000

    fix

--- sql.py
+++ sql.py
@@ -6,7 +6,7 @@ from bot import DB_URI
 
 
 def start() -> scoped_session:
-	engine = create_engine(DB_URI, client_encoding="utf8")
+	engine = create_engine(DB_URI)
 	BASE.metadata.bind = engine
 	BASE.metadata.create_all(engine)
 	return scoped_session(sessionmaker(bind=engine, autoflush=False))
