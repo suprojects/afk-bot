6a961238f0b63e6b1bce499d665f8c840743d376
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-09 14:49:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import telegram.ext as tg
 from config import Config
+import sys
+import os
+from threading import Thread
 
 TOKEN = Config.API_KEY
-OWNER_ID = int(Config.OWNER_ID)
+SUDO_USERS = Config.SUDO_USERS
 OWNER_USERNAME = Config.OWNER_USERNAME
 DB_URI = Config.SQLALCHEMY_DATABASE_URI
 
@@ -15,6 +18,20 @@ def main():
 	from users import USER_HANDLER, USERS_GROUP, BROADCAST_HANDLER, CHATLIST_HANDLER
 	from start import START_HANDLER, HELP_HANDLER, BTN_HANDLER, SL_HANDLER
 	
+	if "-r" in sys.argv:
+		for SUDO_USER in SUDO_USERS:
+			updater.bot.send_message(SUDO_USER, "Bot restarted successfully.")
+
+	def stop_and_restart():
+		os.system("git pull")
+		updater.stop()
+		os.execl(sys.executable, sys.executable, *sys.argv, "-r")
+
+	def restart(update, context):
+		update.message.reply_text("Bot is restarting...")
+		Thread(target = stop_and_restart).start()
+
+	dp.add_handler(CommandHandler("r", restart, filters = Filters.user(SUDO_USERS)))
 	dispatcher.add_handler(START_HANDLER)
 	dispatcher.add_handler(HELP_HANDLER)
 	dispatcher.add_handler(SL_HANDLER)
