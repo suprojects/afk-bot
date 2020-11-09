15460978b39d945846f4ea4ab7a0cc1e1f33e0b3
rojserbest <rojserbest@gmail.com>
2020-11-09 11:54:27 +0000

    fix

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config():
     SUDO_USERS = [951435494, 1178472788]
     API_KEY = "1457211149:AAHk2ZPkBP-u2UNZHoCIeHq6Hc-43KzQaeo"  # my api key, as provided by the botfather
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass@localhost:5432/afkdb'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
