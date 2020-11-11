29eaa9497c0ecb90866b933898dea181780f1bab
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-11 17:08:04 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,75 +1,41 @@
 from bot import dispatcher
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, CallbackQueryHandler
-from lang_sql import *
-from strings import *
 
 bot = dispatcher.bot
 
-
-LANGS = ("کوردیی ناوەندی ☀️", "English 🇺🇸")
-LANGS_KEYBOARD = [[InlineKeyboardButton(LANGS[0], callback_data="ku")],[InlineKeyboardButton(LANGS[1], callback_data="en")]]
-LANGS_REPLY_MARKUP = InlineKeyboardMarkup(LANGS_KEYBOARD)
-
-LANGS_KEYBOARD2 = [[InlineKeyboardButton(LANGS[0], callback_data="ku2")],[InlineKeyboardButton(LANGS[1], callback_data="en2")]]
-LANGS_REPLY_MARKUP2 = InlineKeyboardMarkup(LANGS_KEYBOARD2)
-
 def start(update, context):
-	chat = update.effective_message.chat
-	user = update.effective_user
+	cht = update.effective_message.chat
+	usr = update.effective_user
 	msg = update.effective_message
-	cid = str(chat.id)
 	
-	if chat.type == "private":
+	if cht.type == "private":
 		if "help" in msg.text:
-			init(cid)
-			msg.reply_text(HELP_STRING[msg.text.split("_")[1]].format(user.first_name), parse_mode="HTML")
+			msg.reply_text(
+				"""
+help
+				""".format(user.first_name), parse_mode="HTML")
 		else:
-			if not is_selected(cid):
-				msg.reply_text("Choose language.", reply_markup=LANGS_REPLY_MARKUP2)
-			else:
-				START_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton(START_STRING3[CHAT_LANGS[cid]], url='http://t.me/{}?startgroup=botstart'.format(bot.username))]])
-				msg.reply_text(START_STRING[CHAT_LANGS[cid]], reply_markup=START_REPLY_MARKUP)
+			msg.reply_text("""
+start
+			""")
 	else:
-		init(cid)
 		msg.reply_text(START_STRING2[CHAT_LANGS[cid]])
 
 def help(update, context):
-	chat = update.effective_message.chat
-	user = update.effective_user
+	cht = update.effective_message.chat
+	usr = update.effective_user
 	msg = update.effective_message
-	cid = str(chat.id)
-	init(cid)
+	
 	if chat.type == "private":
-		msg.reply_text(HELP_STRING[CHAT_LANGS[cid]].format(user.first_name), parse_mode="HTML")
+		msg.reply_text("""
+help
+			""".format(user.first_name), parse_mode="HTML")
 	else:
-		HELP_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton(HELP_STRING3[CHAT_LANGS[cid]], url='http://t.me/{}?start=help_{}'.format(bot.username, CHAT_LANGS[cid]))]])
-		msg.reply_text(HELP_STRING2[CHAT_LANGS[cid]], reply_markup=HELP_REPLY_MARKUP)
+		HELP_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton("Help", url='http://t.me/{}?start=help_{}'.format(bot.username, CHAT_LANGS[cid]))]])
+		msg.reply_text("Click the button to get help in PM!", reply_markup=HELP_REPLY_MARKUP)
 
-def lang(update, context):
-	chat = update.effective_message.chat
-	user = update.effective_user
-	msg = update.effective_message
-	cid = str(chat.id)
-	init(cid)
-	update.message.reply_text(LANG_STRING[CHAT_LANGS[cid]], reply_markup=LANGS_REPLY_MARKUP)
 
-def button(update, context):
-	query = update.callback_query
-	query.answer()
-	
-	set_lang(str(query.message.chat.id), query.data.replace("2", ""))
-	
-	if "2" in query.data:
-		START_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton(START_STRING3[query.data.replace("2", "")], url='http://t.me/{}?startgroup=botstart'.format(bot.username))]])
-		query.edit_message_text(START_STRING[query.data.replace("2", "")], reply_markup=START_REPLY_MARKUP)
-	
-	else:
-	
-		STRINGS = {"ku": "زمانی ئەم چاتە کرا بە کوردی.", "en": "The language of this chat was set to English.", "ru": "Я буду говорить по-Русский.", "uz": "Men bundan buyon O‘zbek tilida gaplashaman."}
-		query.edit_message_text(STRINGS[query.data])
 
 START_HANDLER = CommandHandler("start", start)
 HELP_HANDLER = CommandHandler("help", help)
-SL_HANDLER = CommandHandler("lang", lang)
-BTN_HANDLER = CallbackQueryHandler(button)
