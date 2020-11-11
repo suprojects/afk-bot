a2e0d814e4cd4ce2b1a6758a039152f3b2735f23
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-11 17:10:43 +0300

    Update start.py

--- start.py
+++ start.py
@@ -2,8 +2,6 @@ from bot import dispatcher
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import CommandHandler, CallbackQueryHandler
 
-bot = dispatcher.bot
-
 def start(update, context):
 	cht = update.effective_message.chat
 	usr = update.effective_user
@@ -18,7 +16,7 @@ help
 		else:
 			msg.reply_text("""
 start
-			""")
+			""", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Help", url="http://t.me/{}?startgroup=botstart".format(context.bot.username))
 	else:
 		msg.reply_text(START_STRING2[CHAT_LANGS[cid]])
 
@@ -32,7 +30,7 @@ def help(update, context):
 help
 			""".format(user.first_name), parse_mode="HTML")
 	else:
-		HELP_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton("Help", url='http://t.me/{}?start=help_{}'.format(bot.username, CHAT_LANGS[cid]))]])
+		HELP_REPLY_MARKUP = InlineKeyboardMarkup([[InlineKeyboardButton("Help", url="http://t.me/{}?start=help".format(context.bot.username))]])
 		msg.reply_text("Click the button to get help in PM!", reply_markup=HELP_REPLY_MARKUP)
 
 
