a989d335bd856d1bb283862c14e48041e05e75cc
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-13 11:40:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config():
     SUDO_USERS = [951435494, 1178472788]
-    API_KEY = "1457211149:AAHk2ZPkBP-u2UNZHoCIeHq6Hc-43KzQaeo"  # my api key, as provided by the botfather
+    API_KEY = "1431248376:AAEVBYNDZam13E2QAoKgjIcQgs-GDTJ_OvQ"  # my api key, as provided by the botfather
     SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
