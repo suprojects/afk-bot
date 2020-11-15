66020dc4514720f6920539552bb02767d63f0d93
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-15 23:09:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config():
     SUDO_USERS = [951435494, 1178472788]
-    API_KEY = "1431248376:AAEVBYNDZam13E2QAoKgjIcQgs-GDTJ_OvQ"
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///main.db'
+    API_KEY = os.environ.get("TOKEN", "Your TOKEN here")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///main.db" # postgresql db uri
