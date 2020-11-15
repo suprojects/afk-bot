62e86dd627c25915ef72819d8d929f65479739a4
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-15 23:11:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-
 class Config():
     SUDO_USERS = [951435494, 1178472788]
-    API_KEY = os.environ.get("TOKEN", "Your TOKEN here")
+    API_KEY = "1431248376:AAEVBYNDZam13E2QAoKgjIcQgs-GDTJ_OvQ"
     SQLALCHEMY_DATABASE_URI = "sqlite:///main.db" 
