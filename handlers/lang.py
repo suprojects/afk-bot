415f8081f3e71a45b6e255eb9237e8a08382907e
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-16 13:37:04 +0300

    Update lang.py

--- handlers/lang.py
+++ handlers/lang.py
@@ -8,6 +8,8 @@ from strings import get_languages, get_string
 
 from il import il
 
+from config import Config
+
 
 def language_buttons(languages):
     buttons = []
@@ -26,7 +28,7 @@ def change_language(update, context, lang):
     cht, usr, msg = update.effective_chat, update.effective_user, update.effective_message
 
     if cht.type != "private":
-        if cht.get_member(usr.id).status not in ("creator", "administrator"):
+        if cht.get_member(usr.id).status not in ("creator", "administrator") or usr.id bot in Config.SUDO_USERS:
             return
 
     languages = get_languages()
