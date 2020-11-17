97799a8763d83374433bafdcf9bdca1a28d3fad0
Roj <49933115+rojserbest@users.noreply.github.com>
2020-11-17 16:09:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
 
+if os.environ.get("TOKEN", "N") == "N" or os.environ.get("DATABASE_URL", "N") == "N":
+    print("Please specify TOKEN and DATABASE_URL environment variables before starting the bot.")
+    exit()
+
 class Config():
     SUDO_USERS = [951435494, 1178472788]
-    API_KEY = "1453748650:AAHkjH_5E_NF8QmmlseKxjB-DXLALQz5-EE"
+    API_KEY = os.environ.get("TOKEN")
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
