f8631e8cac2a7cc45e210792fea33cc47ae2ca86
Roj <n00b@localhost:5931>
2020-12-21 23:01:23 +0300

    x

--- sql/__init__.py
+++ sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from bot import DB_URI
+from secrets import DB_URI
 
 
 def start() -> scoped_session:
