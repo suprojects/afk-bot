893aa6d0b69c3228ab02ad3c37342e1bdaa79178
Roj <49933115+rojserbest@users.noreply.github.com>
2020-12-21 18:46:44 +0300

    Create clean.py

---
+++ handlers/clean.py
@@ -0,0 +1,22 @@
+from telegram.ext import CommandHandler
+from sql.users_sql import get_all_chats, del_chat
+import config
+
+
+def clean(update, context, lang):
+    chats = [chat.id for chat in get_all_chats()]
+    
+    for chat in chats:
+        try:
+            context.bot.get_chat(chat)
+        except:
+            try:
+                del_chat(chat)
+            except:
+                pass
+    update.message.reply_text("Database cleaning finished.")
+
+
+__handlers__ = [
+    [CommandHandler("clean", clean, filters=Filters.user(config.Config.SUDO_USERS))]
+]
